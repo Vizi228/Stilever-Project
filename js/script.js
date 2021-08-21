bd7e5a9e9eb316ff54990efb837923230eb17f3d
Vizi228 <lalka210767@gmail.com>
2021-08-22 00:26:13 +0300

    version 1.04

--- js/script.js
+++ js/script.js
@@ -55,6 +55,65 @@ new Swiper('.content-body__slider', {
 	//},
 });
 
+
+
+new Swiper('.account-main__slider', {
+	/*
+	effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	*/
+	slidesPerView: 1,
+	spaceBetween: 32,
+	speed: 800,
+    direction: 'vertical',
+	//touchRatio: 0,
+	//simulateTouch: false,
+	//loop: true,
+	//preloadImages: false,
+	lazy: true,
+	parallax: true,
+	// Dotts
+	 pagination: {
+		el: '.account-main__pagination',
+		clickable: true,
+	},
+	// Arrows
+
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	//scrollbar: {
+	//	el: '.swiper-scrollbar',
+	//},
+});
+
 document.addEventListener('click', documentActions);
 
 
@@ -64,12 +123,19 @@ function documentActions(e) {
 	if(targetElement.classList.contains('inner-menu__search_button')) {
 		menuInput.classList.toggle('_active');
 	}
+	let changedPage = document.querySelector('.main__body');
+	let getPage = document.querySelector('.main__account')
+	if(targetElement.classList.contains('page-swap-button')){
+		changedPage.classList.add('_active');
+		getPage.classList.add('_active')
+	}
 	if(document.querySelector('.main-body__button')){
 		const mainPopup = document.querySelector('.popup')
 		if(targetElement.classList.contains('popup__button')) {
 			mainPopup.classList.add('_active')
 		}	
 	}
+
 }
 
 if(document.querySelectorAll('.inner-menu__settings')) {
@@ -80,4 +146,5 @@ if(document.querySelectorAll('.inner-menu__settings')) {
 	})
 })
 }
+
 
