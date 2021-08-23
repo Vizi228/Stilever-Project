9fe56a66a087b6ef53c5892f3d96e73b95e05d76
Vizi228 <lalka210767@gmail.com>
2021-08-23 21:47:02 +0300

    version 1.05

--- js/script.js
+++ js/script.js
@@ -1,4 +1,106 @@
-new Swiper('.content-body__slider', {
+function renderMain() {
+	new Swiper('.content-body__slider', {
+		/*
+		effect: 'fade',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		*/
+		slidesPerView: 1,
+		spaceBetween: 32,
+		speed: 800,
+		direction: 'vertical',
+		//touchRatio: 0,
+		//simulateTouch: false,
+		//loop: true,
+		//preloadImages: false,
+		lazy: true,
+		parallax: true,
+		// Dotts
+		 pagination: {
+			el: '.slider-content__pagination',
+			clickable: true,
+		},
+	
+		/*
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 0,
+				autoHeight: true,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			1268: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		},
+		*/
+	});
+	
+}
+
+ 
+
+function renderAccount() {
+	new Swiper('.account-main__slider', {
+		/*
+		effect: 'fade',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		*/
+		slidesPerView: 1,
+		spaceBetween: 32,
+		speed: 800,
+		direction: 'vertical',
+		//touchRatio: 0,
+		//simulateTouch: false,
+		//loop: true,
+		//preloadImages: false,
+		// Dotts
+		lazy: true,
+		parallax: true,
+		pagination: {
+			el: '.account-main__pagination',
+			clickable: true,
+		},
+		// Arrows
+	
+		/*
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 0,
+				autoHeight: true,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			1268: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		},
+		*/
+	});
+}
+
+new Swiper('.banner__slider', {
 	/*
 	effect: 'fade',
 	autoplay: {
@@ -9,18 +111,21 @@ new Swiper('.content-body__slider', {
 	slidesPerView: 1,
 	spaceBetween: 32,
 	speed: 800,
-    direction: 'vertical',
 	//touchRatio: 0,
 	//simulateTouch: false,
 	//loop: true,
 	//preloadImages: false,
+	// Dotts
 	lazy: true,
 	parallax: true,
-	// Dotts
-	 pagination: {
-		el: '.slider-content__pagination',
+	pagination: {
+		el: '.banner__pagination',
 		clickable: true,
 	},
+	navigation: {
+    nextEl: '.banner__button-next',
+    prevEl: '.banner__button-prev',
+  },
 	// Arrows
 
 	/*
@@ -44,75 +149,13 @@ new Swiper('.content-body__slider', {
 		},
 	},
 	*/
-	on: {
-		lazyImageReady: function () {
-			ibg();
-		},
-	}
-	// And if we need scrollbar
-	//scrollbar: {
-	//	el: '.swiper-scrollbar',
-	//},
 });
 
 
 
-new Swiper('.account-main__slider', {
-	/*
-	effect: 'fade',
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	*/
-	slidesPerView: 1,
-	spaceBetween: 32,
-	speed: 800,
-    direction: 'vertical',
-	//touchRatio: 0,
-	//simulateTouch: false,
-	//loop: true,
-	//preloadImages: false,
-	lazy: true,
-	parallax: true,
-	// Dotts
-	 pagination: {
-		el: '.account-main__pagination',
-		clickable: true,
-	},
-	// Arrows
 
-	/*
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
-		},
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 20,
-		},
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 20,
-		},
-		1268: {
-			slidesPerView: 4,
-			spaceBetween: 30,
-		},
-	},
-	*/
-	on: {
-		lazyImageReady: function () {
-			ibg();
-		},
-	}
-	// And if we need scrollbar
-	//scrollbar: {
-	//	el: '.swiper-scrollbar',
-	//},
-});
+
+
 
 document.addEventListener('click', documentActions);
 
@@ -125,9 +168,15 @@ function documentActions(e) {
 	}
 	let changedPage = document.querySelector('.main__body');
 	let getPage = document.querySelector('.main__account')
-	if(targetElement.classList.contains('page-swap-button')){
+	if(targetElement.classList.contains('account-swap-button')){
 		changedPage.classList.add('_active');
 		getPage.classList.add('_active')
+		renderAccount();
+	}
+	if(targetElement.classList.contains('main-swap-button')){
+		changedPage.classList.remove('_active');
+		getPage.classList.remove('_active')
+		renderMain();
 	}
 	if(document.querySelector('.main-body__button')){
 		const mainPopup = document.querySelector('.popup')
@@ -137,6 +186,28 @@ function documentActions(e) {
 	}
 
 }
+
+window.onload = () => {
+		let nickname = document.querySelectorAll('.inner-menu__nickname');
+		
+		nickname.forEach(change => {
+			let text = change.innerText;
+			const fullNickname = document.createElement(`span`)
+			fullNickname.innerText = text;
+			fullNickname.style.cssText = `display:none;`;
+			change.appendChild(fullNickname);
+			change.addEventListener('mouseover', () => {
+				fullNickname.style.cssText = `display:block;`;
+			})
+			change.addEventListener('mouseout', () => {
+				fullNickname.style.cssText = `display:none;`;
+			})
+		})
+}
+
+
+
+
 
 if(document.querySelectorAll('.inner-menu__settings')) {
 	const changesButton = document.querySelectorAll('.inner-menu__settings');
